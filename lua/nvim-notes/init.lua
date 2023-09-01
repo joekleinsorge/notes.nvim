a5bda3b42029706ceafb1dadf4be0d9dba05aac9
Joe Kleinsorge <joe@kleinsorge.dev>
2023-08-31 22:00:08 -0400

    ci: commit

--- lua/nvim-notes/init.lua
+++ lua/nvim-notes/init.lua
@@ -1,26 +1,24 @@
 local M = {}
 
-M.find_or_create_note = function()
+M.find_note = function()
   local telescope = require("telescope.builtin")
   local notes_path = "~/git/notes/vault"
 
   telescope.find_files({
-    prompt_title = "Find or Create Note",
+    prompt_title = "Find Note",
     cwd = notes_path,
-    attach_mappings = function(_, map)
-      map("i", "<CR>", function(bufnr)
-        local name = vim.fn.input("Note Name: ")
-        if name == "" then
-          name = os.date("daily.%Y-%m-%d.md")
-        end
-        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { name })
-        vim.fn.execute("write")
-        vim.cmd("edit " .. name)
-        require("telescope.actions").close(bufnr)
-      end)
-      return true
-    end,
   })
+end
+
+M.create_note = function()
+  local name = vim.fn.input("Note Name: ")
+  if name == "" then
+    name = os.date("daily.%Y-%m-%d.md")
+  end
+  local notes_path = "~/git/notes/vault"
+  local full_path = notes_path .. "/" .. name
+  vim.fn.writefile({}, full_path)
+  vim.cmd("e " .. full_path)
 end
 
 M.search_notes = function()
